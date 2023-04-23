08ecda698febec1d167c7df046b4a5833f27c8b9
Veticia <668573+Veticia@users.noreply.github.com>
2023-04-23 21:53:23 +0200

    improve copy script by adding .png if it wasnt provided

--- copy_images.sh
+++ copy_images.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-cp -v "$SCRIPT_DIR/icons/$1" "$SCRIPT_DIR/icons/$2"
-cp -v "$SCRIPT_DIR/banners/$1" "$SCRIPT_DIR/banners/$2"
-cp -v "$SCRIPT_DIR/tenaldo_square/$1" "$SCRIPT_DIR/tenaldo_square/$2"
+# append .png to a filename if it's not present
+function add_png {
+    filename=$1
+    if [[ ! "$filename" =~ \.png$ ]]; then
+        filename="${filename}.png"
+    fi
+    echo "$filename"
+}
+
+old=$(add_png "$1")
+new=$(add_png "$2")
+
+cp -v "$SCRIPT_DIR/icons/$old" "$SCRIPT_DIR/icons/$new"
+cp -v "$SCRIPT_DIR/banners/$old" "$SCRIPT_DIR/banners/$new"
+cp -v "$SCRIPT_DIR/tenaldo_square/$old" "$SCRIPT_DIR/tenaldo_square/$new"
