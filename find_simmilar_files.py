1465ddfc89bcf307b482bcd84caf548781a13d38
Veticia <668573+Veticia@users.noreply.github.com>
2023-04-23 19:56:31 +0200

    make finding simmilar filenames faster

--- find_simmilar_files.py
+++ find_simmilar_files.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import os
-import difflib
+import Levenshtein
 import sys
 import time
-from threading import Thread
+from multiprocessing import Process, Queue
 
 def format_time(seconds):
     intervals = (
@@ -15,7 +15,7 @@ def format_time(seconds):
         ('minutes', 60),
         ('seconds', 1)
     )
-    
+
     result = []
     for name, count in intervals:
         value = int(seconds // count)
@@ -30,14 +30,30 @@ def possible_pairs(n):
     pairs = (n * (n-1)) // 2
     return pairs
 
+def compare_pairs(folder_path, pairs, result_queue):
+    for pair in pairs:
+        i, j = pair
+        file1 = files[i]
+        file2 = files[j]
+        file1_name, _ = os.path.splitext(file1)
+        file2_name, _ = os.path.splitext(file2)
+        ratio = Levenshtein.ratio(file1_name, file2_name)
+        if ratio > 0.8:
+            result_queue.put((file1, file2))
+
 def find_similar_files(folder_path):
+    global files
     files = os.listdir(folder_path)
     similar_files = []
+    pairs = []
     total = possible_pairs(len(files))
+    sys.stdout.write(f'pairs to check {total}\n')
+    sys.stdout.flush()
     checked = 0
     start_time = time.time()
     for i in range(len(files)):
         for j in range(i+1, len(files)):
+            pairs.append((i, j))
             checked = checked + 1
             percent = (checked / total) * 100
             elapsed_time = time.time() - start_time
@@ -47,12 +63,19 @@ def find_similar_files(folder_path):
                 eta = 0
             sys.stdout.write(f'\rComparing file {i+1} of {len(files)} ({percent:.2f}%), ETA: {eta}')
             sys.stdout.flush()
-            file1_name, _ = os.path.splitext(files[i])
-            file2_name, _ = os.path.splitext(files[j])
-            ratio = difflib.SequenceMatcher(None, file1_name, file2_name).ratio()
-            if ratio > 0.8:
-                similar_files.append((files[i], files[j]))
     print()
+    result_queue = Queue()
+    chunk_size = 100000
+    chunks = [pairs[i:i + chunk_size] for i in range(0, len(pairs), chunk_size)]
+    processes = []
+    for chunk in chunks:
+        process = Process(target=compare_pairs, args=(folder_path, chunk, result_queue))
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
+    while not result_queue.empty():
+        similar_files.append(result_queue.get())
     return similar_files
 
 script_path = os.path.abspath(__file__)
