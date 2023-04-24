f00f3936d35fb46a569713bd84ca04f65bccdf16
Veticia <668573+Veticia@users.noreply.github.com>
2023-04-24 11:18:46 +0200

    compare current image with its git stored variant

---
+++ compare_with_git.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import subprocess
+import os
+from PIL import Image, ImageChops
+import io
+import imghdr
+
+def compare_images():
+    # Get the path of the repository
+    repo_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
+    # Get the list of changed files
+    changed_files = subprocess.check_output(['git', 'diff', '--name-only', 'HEAD']).strip().decode('utf-8').split('\n')
+    for rel_path in changed_files:
+        # Get the absolute path of the image
+        image_path = os.path.join(repo_path, rel_path)
+        # Check if the file exists
+        if not os.path.exists(image_path):
+            continue
+        # Check if the file is an image
+        if imghdr.what(image_path) is None:
+            continue
+        # Get the latest commit hash
+        latest_commit = subprocess.check_output(['git', 'rev-list', '-1', 'HEAD', rel_path]).strip().decode('utf-8')
+        # Get the image from the latest commit
+        old_image_data = subprocess.check_output(['git', 'show', f'{latest_commit}:{rel_path}'])
+        old_image = Image.open(io.BytesIO(old_image_data))
+        # Convert to RGB
+        old_image = old_image.convert('RGB')
+        # Resize the image
+        old_image = old_image.resize((128, 128))
+        # Open the current version of the image
+        current_image = Image.open(image_path)
+        # Convert to RGB
+        current_image = current_image.convert('RGB')
+        # Resize the image
+        current_image = current_image.resize((128, 128))
+        # Create a diff image
+        diff_image = ImageChops.difference(current_image, old_image)
+        # Check if the diff image is empty
+        if diff_image.getbbox():
+            # Save the diff image
+            diff_image.save(f'{rel_path}#diff.png')
+        else:
+            # Check if the dimensions of the current image are smaller or equal to those of the old image
+            if current_image.width <= old_image.width and current_image.height <= old_image.height:
+                # Revert to unmodified version of the file
+                with open(image_path, 'wb') as f:
+                    f.write(old_image_data)
+
+# Example usage
+compare_images()
